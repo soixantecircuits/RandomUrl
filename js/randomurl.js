9c6a3f3c49ddb89324966e45eb06b0abadedc6a5
Gabrielstuff <gabriel@soixantecircuits.fr>
2012-01-03 14:43:21 +0100

    separate data from code

--- js/randomurl.js
+++ js/randomurl.js
@@ -2,35 +2,6 @@
 
 // Load the application once the DOM is ready, using `jQuery.ready`:
 $(function(){
-
-  // Setup
-  window.soixanteprojects = [ {url:'http://www.fondationchirac.eu'},
-                              {url:'http://www.afinejewel.com'},
-                              {url:'http://www.dailymotion.com/video/x83zi8_rm001-pour-remy-martin_creation'},
-                              {url:'http://www.cpp-luxury.com/en/christian-dior-inaugurate-flagship-in-beijing-with-lady-dior-exhibit_1942.html'},
-                              {url:'http://itunes.apple.com/us/app/leauparkenzo/id434401875?mt=8'},
-                              {url:'http://www.ensci.com/fr/masteres-specialises/creation-et-technologie-contemporaine/presentation/mission/'},
-                              {url:'http://babiole.net/spip.php?article62'},
-                              {url:'http://www.licence.elec.upmc.fr/fr/02_Licences_alternance/04-L3_IOVIS/'},
-                              {url:'http://www.celsa-misc.fr/'},
-                              {url:'http://itunes.apple.com/us/app/pwc-story/id408374360?mt=8'},
-                              {url:'http://www.collectifr.fr/1pourcent/c-rivalin-f-bragigand'},
-                              {url:'http://franklinazzi.com/dotclear/index.php?renard'},
-                              {url:'http://babiole.net/spip.php?article5'},
-                              {url:'http://www.leguide.ma/voyages/voy-articles.cfm?prod=197'},
-                              {url:'http://www.artdaily.com/index.asp?int_sec=11&int_new=33583&int_modo=2'},
-                              {url:'http://franklinazzi.com/dotclear/index.php?ratp'},
-                              {url:'http://babiole.net/spip.php?article4'},
-                              {url:'http://www.dailymotion.com/video/x83zm1_section-amour-ballons-d-amour_creation'},
-                              {url:'http://www.dailymotion.com/video/x83zf1_magnifique-de-lancome-ecran-de-led_creation#rel-page-under-4'},
-                              {url:'http://www.babiole.net/spip.php?article9'},
-                              {url:'http://www.be-art-website.com/index.php?in=10198'},
-                              {url:'http://www.galeriefrankelbaz.com/wp/artists/davide-balula/davide-balula-shows/#DB-show2007-4'},
-                              {url:'http://bouchrajarrar.com/'}
-                              //{url:''},
-                              //{url:''},
-                              //{url:''},
-                            ]; 
   
   //  Model
   // ----------
