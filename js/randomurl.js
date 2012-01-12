a58aac57b8aafae0b5157d612c16290e57c17111
Emmanuel Geoffray <manuel@soixantecircuits.fr>
2012-01-12 12:46:32 +0100

    Categories are now clickable

--- js/randomurl.js
+++ js/randomurl.js
@@ -27,34 +27,73 @@ $(function(){
 
     initialize: function() {
       projects.add(soixanteprojects);
-      $("#logo a").bind('click', function(e){
+      $("#logo img").bind('click', function(e){
         e.preventDefault();
-        
         $("#url").fadeOut('slow',function(){
           App.randProject();          
         });
+      });
 
+
+      $("#scenographie").bind('click', function(e){
+        e.preventDefault();
+        $("#url").fadeOut('slow',function(){
+          App.randProject('scenographie');
+        });
+      });
+      $("#enseignement").bind('click', function(e){
+        e.preventDefault();
+        $("#url").fadeOut('slow',function(){
+          App.randProject('enseignement');
+        });
+      });
+      $("#application").bind('click', function(e){
+        e.preventDefault();
+        $("#url").fadeOut('slow',function(){
+          App.randProject('application');
+        });
+      });
+      $("#internet").bind('click', function(e){
+        e.preventDefault();
+        $("#url").fadeOut('slow',function(){
+          App.randProject('internet');
+        });
       });
       this.randProject();
     },
 
-    randProject: function(){
+    randProject: function(type){
       
       if(projects.length < 1){
         console.log("GOOD");
         projects.add(soixanteprojects);
       }
-
-      var rand = Math.floor(Math.random()*projects.length);
-      this.addOne( projects.at(rand));    
-      projects.remove( projects.at(rand)); 
+      
+      if (type){
+        var projectstype = _.filter(projects.models, function(proj){ return proj.get('type') == type;});
+        if (projectstype.length == 0){
+          projectstype = _.filter(soixanteprojects, function(proj){ return proj.type == type;});
+          projects.add(projectstype);
+          projectstype = _.filter(projects.models, function(proj){ return proj.get('type') == type;});
+        }
+        if (projectstype.length > 0){
+          var rand = Math.floor(Math.random()*projectstype.length);
+          this.addOne( projectstype[rand]);    
+          projects.remove(projectstype[rand]); 
+        }
+      } 
+      else { 
+        var rand = Math.floor(Math.random()*projects.length);
+        this.addOne( projects.at(rand));    
+        projects.remove( projects.at(rand));
+      } 
     },
 
     addOne: function(proj) {
         $("#url").html('<iframe src="' + proj.get('url') + '" frameborder="0" marginheight="0" marginwidth="0" width="100%" height="100%" scrolling="auto"><a href="' + proj.get('url') + '">' + proj.get('url') + '</a></iframe>');          
         $("#url").fadeIn("5500");
         $(".blue").removeClass("blue");
-        $("."+proj.get('type')).addClass("blue");
+        $("#"+proj.get('type')).addClass("blue");
         Cufon.refresh();
     }
 
